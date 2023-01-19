e72ef056337011519546ce9c01b543646c945fd7
Adijat Ojutomori <43005376+toussyn@users.noreply.github.com>
2023-01-19 22:20:17 +0100

    Update deploy.js

--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -16,7 +16,7 @@ async function main() {
   console.log("Contract deployed to:", priceConsumer.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
+// We recommend this pattern to be able to use async or await everywhere
 // and properly handle errors.
 main().catch((error) => {
   console.error(error);
